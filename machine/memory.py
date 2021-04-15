b7f29645eff53717694f1cfdf2ccf03b928cce46
Jorge Morales <jomorales@unal.edu.co>
2021-04-14 22:39:26 -0500

    test

---
+++ machine/memory.py
@@ -0,0 +1,4 @@
+class Memory:
+    def hello(self, n):
+        for i in range(n):
+            print(i)
---
+++ main.py
@@ -0,0 +1,5 @@
+from machine.memory import Memory
+
+if __name__ == '__main__':
+    a = Memory()
+    a.hello(3)
